40818ed089401522e616c22b0ca4c04d2c700601
Iain McCoy <iain@mccoy.id.au>
2019-12-06 02:08:12 +1100

    works I guess

--- src/day4a.rs
+++ src/day4a.rs
@@ -1,5 +1,8 @@
 use std::io;
 
+const MIN : [i32;6] = [0;6];
+const MAX : [i32;6] = [9;6];
+
 fn digits_from_num(num : i32) -> [i32;6] {
     let mut num = num;
     let mut digits : [i32;6] = [0;6];
@@ -12,36 +15,54 @@ fn digits_from_num(num : i32) -> [i32;6] {
     return digits;
 }
 
-fn count_combinations_0() -> i32 {
-    let mut count = 0;
-    for digit in 0..10 {
-        count += count_combinations(digit, digit, 1, false);
-    }
-    return count;
-}
-
-fn count_combinations(number : i32, current_digit : i32, place : i32, have_repeated : bool) -> i32 {
+fn count_combinations(number : i32, min: &[i32], max: &[i32], previous_digit : i32, place : i32, have_repeated : bool) -> i32 {
     if place == 5 {
         // if we're in the last position, and we haven't repeated a digit yet, we'll have to repeat the previous digit.
         // but if we've already covered the repeat-a-digit requirement, we'll have a bunch of options here
         if have_repeated {
-            println!("{}x", number);
-            return 10 - current_digit;
+            println!("{}x ({})", number, max[0] - previous_digit + 1);
+            return max[0] - previous_digit + 1;
         } else {
-            println!("{}{}", number, current_digit);
+            println!("{}{}", number, previous_digit);
             return 1;
         }
     } else {
-        let mut count = 0;
-        count += count_combinations(number * 10 + current_digit, current_digit, place + 1, true);
-        for next_digit in (current_digit+1)..10 {
-            count += count_combinations(number * 10 + next_digit, next_digit, place + 1, have_repeated);
+        println!("{} <= n <= {}", min[0], max[0]);
+        if min[0] == max[0] {
+            let current_digit = min[0];
+            return count_combinations(number * 10 + current_digit, &min[1..], &max[1..], current_digit, place + 1, have_repeated || (current_digit == previous_digit));
+        } else {
+            if (min[0] >= previous_digit) {
+                let mut count = 0;
+                count += count_combinations(number * 10 + min[0], &min[1..], &MAX, min[0], place + 1, (place != 0 && min[0] == previous_digit) || have_repeated);
+                if max[0] - min[0] > 1 {
+                    for current_digit in (min[0]+1)..max[0] {
+                        count += count_combinations(number * 10 + current_digit, &MIN, &MAX, current_digit, place + 1, (place != 0 && current_digit == previous_digit) ||have_repeated);
+                    }
+                }
+                count += count_combinations(number * 10 + max[0], &MIN, &max[1..], max[0], place + 1, (place != 0 && max[0] == previous_digit) || have_repeated);
+                return count;
+            } else if (previous_digit < max[0]) {
+                let mut count = 0;
+                count += count_combinations(number * 10 + previous_digit, &MIN, &MAX, previous_digit, place + 1, true);
+                if max[0] - previous_digit > 1 {
+                    for current_digit in (previous_digit+1)..max[0] {
+                        count += count_combinations(number * 10 + current_digit, &MIN, &MAX, current_digit, place + 1, (place != 0 && current_digit == previous_digit) ||have_repeated);
+                    }
+                }
+                count += count_combinations(number * 10 + max[0], &MIN, &max[1..], max[0], place + 1, (place != 0 && max[0] == previous_digit) || have_repeated);
+                return count;
+            } else if (previous_digit == max[0]) {
+                let current_digit = max[0];
+                return count_combinations(number * 10 + current_digit, &MIN, &max[1..], current_digit, place + 1, have_repeated || (current_digit == previous_digit));
+            } else {
+                return 0;
+            }
         }
-        return count;
     }
 }
 
 fn main() -> io::Result<()> {
-    println!("{}", count_combinations_0());
+    println!("{}", count_combinations(0, &[1,3,7,6,8,3], &[5,9,6,2,5,3], 0, 0, false));
     Ok(())
 }
