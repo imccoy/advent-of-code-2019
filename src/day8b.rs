a4f148b93c386c552634b7744fb7417e3217d977
Iain McCoy <iain@mccoy.id.au>
2019-12-08 23:07:54 +1100

    try to do it with one less allocation

--- src/day8b.rs
+++ src/day8b.rs
@@ -23,5 +23,26 @@ fn main() -> io::Result<()> {
         let line_string : String = line.iter().collect();
         println!("{}", line_string);
     }
+
+
+    // just for giggles, do it again but without explicitly collecting the layers into a vec
+    let mut result : [char;150] = ['2';150];
+    for layer in digits.chunks(image_size) {
+        for (index, value) in layer.iter().enumerate() {
+            if result[index] == '2' {
+                result[index] = match *value {
+                                    '0' => ' ',
+                                    '1' => 'X',
+                                    _   => '2'
+                                }
+            }
+        }
+    }
+
+    for line in result.chunks(width) {
+        let line_string : String = line.iter().collect();
+        println!("{}", line_string);
+    }
+
     Ok(())
 }
